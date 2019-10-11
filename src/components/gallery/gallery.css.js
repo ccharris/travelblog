7bde920fccc67db8725288c70bf270e86403a0a5
Carolyn Harris <ccharris27@gmail.com>
2019-10-11 11:48:44 -0400

    header and home styles

--- src/components/gallery/gallery.css.js
+++ src/components/gallery/gallery.css.js
@@ -7,7 +7,15 @@ export const Container = styled.div`
   grid-gap: 4rem;
   padding: 0 4rem;
   margin: 2rem 0;
-
+  a {
+    color: #002b34;
+    transition: color 0.2s ease;
+    text-decoration: none;
+    margin-right: 8px;
+    &:hover {
+      color: inherit;
+    }
+  }
   ${MEDIA.TABLET`
     display: block;
   `};
--- src/components/header/header.css.js
+++ src/components/header/header.css.js
@@ -2,14 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   padding: 4rem;
+  background-color: #006b80;
 
   a {
-    color: #757575;
+    color: white;
     transition: color 0.2s ease;
     text-decoration: none;
+    margin-right: 8px;
 
     &:hover {
       color: inherit;
